0c61c0b226b3491f78222ac1638b109b1398b554
Elimas1990 <estebanlimas@gmail.com>
2021-03-18 22:05:33 -0300

    se agrego bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-new-proyect';
-  primeraEdad:number;
-  segundaEdad:number;
-  promedio:string;
-  suma:string;
-  Calcular(){
-    let sumatoria
-    sumatoria=this.primeraEdad+this.segundaEdad
-    this.suma='Suma = '+sumatoria
-    this.promedio='Promedio = '+sumatoria/2
-    
-  }
-  Limpiar(){
-    this.primeraEdad=null;
-    this.segundaEdad=null;
-    this.suma=null;
-    this.promedio=null;
-  }
+ 
 }
--- src/app/componentes/bienvenido/bienvenido.component.ts
+++ src/app/componentes/bienvenido/bienvenido.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bienvenido.component.css']
 })
 export class BienvenidoComponent implements OnInit {
-
+  title = 'Ejercitación';
+  primeraEdad:number;
+  segundaEdad:number;
+  promedio:string;
+  suma:string;
+  Calcular(){
+    let sumatoria
+    sumatoria=this.primeraEdad+this.segundaEdad
+    this.suma='Suma = '+sumatoria
+    this.promedio='Promedio = '+sumatoria/2
+    
+  }
+  Limpiar(){
+    this.primeraEdad=null;
+    this.segundaEdad=null;
+    this.suma=null;
+    this.promedio=null;
+  }
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
